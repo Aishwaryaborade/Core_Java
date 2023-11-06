8c4c19fbc5e1a1eacc87d6ce69580321f408992c
Aishwaryaborade <103181122+Aishwaryaborade@users.noreply.github.com>
2023-11-06 15:16:16 +0530

    Add files via upload

---
+++ EnumDemo/Days.java
@@ -0,0 +1,16 @@
+package enumDemo;
+
+public class Days {
+	//enum is used to declare constant
+	//constant value cannot be change
+public enum day
+{
+	Monday,Tuesday, Wednesday, Thursday, Friday,Saturday, Sunday
+}
+	public static void main(String[] args) {
+	
+		System.out.println(day.Sunday);
+		System.out.println(day.Thursday);
+	}
+
+}
